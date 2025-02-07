5c787d0867e536aab3c40c48c990749642d2ebb8
SantiagoMonG <santiagomong@gmail.com>
2025-02-06 22:52:49 -0500

    Updated Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,9 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.ico" /> {/* ✅ Add favicon link */}
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background dark:bg-gray-900 text-textDark dark:text-white`}>
         <ThemeProvider attribute="class">
           {children}
